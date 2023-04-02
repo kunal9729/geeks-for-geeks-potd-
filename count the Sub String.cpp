61509ea558c08da1b26b37ed3b432190fb0f4bfe
Kunal Sabharwal <ksabharwal77@gmail.com>
2023-04-02 23:47:38 +0530

    Create count the Sub String.cpp

---
+++ count the Sub String.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    int countSubstring(string S)
+    {
+        // code here
+        
+        int cnt=0; 
+        int okk = 0;
+        for(int i=0; i<S.length(); i++){
+            cnt=0;
+            for(int j=i; j<S.length();j++){
+                if(S[j]>='a' && S[j]<='z'){
+                    cnt++;
+                }
+                else
+                cnt--;
+                
+                if(cnt==0)
+                okk++;
+            }
+        }
+        
+        return okk;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin>>S;
+        Solution obj;
+        cout<<obj.countSubstring(S)<<endl;
+    }
+}  
+// } Driver Code Ends
