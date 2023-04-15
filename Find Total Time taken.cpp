fdb95f7ed1f5ef2f95924e19d1235aeaa170c4bf
Kunal Sabharwal <ksabharwal77@gmail.com>
2023-04-15 23:33:55 +0530

    Create Find Total Time taken.cpp

---
+++ Find Total Time taken.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int totalTime(int n, vector<int> &arr, vector<int> &time) {
+        int res = -1;
+        unordered_map<int, int> countMap;
+        for(auto a : arr) {
+            countMap[a]++;
+            if(countMap[a] > 1) {
+                res += time[a-1];
+            }
+            else {
+                res++;
+            }
+        }
+        
+        return res;
+        // code here
+    }
+};
+
+//T.c O(n)
+//S.c O(k) -> k constant for distinct elements in 'arr' vector
+
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<int> arr(n);
+        Array::input(arr,n);
+        
+        
+        vector<int> time(n);
+        Array::input(time,n);
+        
+        Solution obj;
+        int res = obj.totalTime(n, arr, time);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
