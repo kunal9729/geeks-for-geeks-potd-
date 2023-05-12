18301806f77f60af5ae08766008a5261621e0e2c
Kunal Sabharwal <ksabharwal77@gmail.com>
2023-05-12 20:36:15 +0530

    Create Array Operations.cpp

---
+++ Array Operations.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+public:
+    int arrayOperations(int n, vector<int>& arr) {
+        int ans = 0;
+        int length = 0;
+    
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                if (length) {
+                    ans += 1;
+                }
+                length = 0;
+            }
+            else {
+                length += 1;
+            }
+        }
+    
+        if (length == n) {
+            return -1;
+        }
+        if (length) {
+            ans += 1;
+        }
+    
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        
+        int n;
+        cin>>n;
+        
+        
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        
+        Solution obj;
+        int res = obj.arrayOperations(n, arr);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
