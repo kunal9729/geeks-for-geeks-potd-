fb21c9e99f23f5824329fd810747e959e1041a15
Kunal Sabharwal <ksabharwal77@gmail.com>
2023-05-15 22:28:38 +0530

    Create count Total Setbits.cpp

---
+++ count Total Setbits.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+    long long calculateSum(long long x) {
+        long long sum = (x * 1ll * pow(2, x - 1) * 1ll) + 1;
+        return sum;
+    }
+    
+public:
+    long long countBits(long long n) {
+        long long sum = 0;
+        
+        while (n) {
+            long long exponent = log2(n);
+            
+            if (sum != 0)
+                sum += n;
+                
+            sum += calculateSum(exponent);
+            n -= pow(2, exponent);
+        }
+        
+        return sum;
+    }
+};
+
