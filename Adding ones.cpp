12757b2320161fdeaff3fa9f63cf9ce69d88cc96
Kunal Sabharwal <ksabharwal77@gmail.com>
2023-06-11 23:05:51 +0530

    Create Adding ones.cpp

---
+++ Adding ones.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+public:
+    void update(int a[], int n, int updates[], int k)
+    {
+        vector<int> frequency(n, 0);
+        
+        for(int i = 0; i < k; i++)
+            frequency[updates[i] - 1]++;
+            
+        int cumulativeAddition = 0;
+        
+        for(int i = 0; i < n; i++){
+            cumulativeAddition += frequency[i];
+            a[i] += cumulativeAddition;
+        }
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int t; cin>>t;
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;
+		int a[n]={0}, updates[k]={0};
+		for(int i = 0; i < k; i++)
+		cin>>updates[i];
+		Solution ob;
+		ob.update(a, n, updates, k);
+		
+		for(int i = 0; i < n; i++)
+		cout<<a[i]<<" ";
+		
+		cout<<endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
