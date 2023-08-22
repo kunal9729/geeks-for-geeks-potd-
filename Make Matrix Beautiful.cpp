462e5a7dc9ccc203c255c7ff4b48af391b2b38d5
Kunal Sabharwal <ksabharwal77@gmail.com>
2023-08-22 21:28:43 +0530

    Create Make Matrix Beautiful.cpp

---
+++ Make Matrix Beautiful.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+    public:
+    //Function to find minimum number of operations that are required 
+    //to make the matrix beautiful.
+    int findMinOpeartion(vector<vector<int>> matrix, int n)
+    {
+        vector<int> values(n);
+        int need = 0;
+        
+        for(int i = 0; i < n; i++){
+            int row, col;
+            row = col = 0;
+            
+            for(int j = 0; j < n; j++){
+                row += matrix[j][i];
+                col += matrix[i][j];
+            }
+            
+            need = max({need, row, col});
+            values[i] = row;
+        }
+        
+        int ans = 0;
+        for(auto i : values)
+            ans += need - i;
+            
+        return ans;
+    } 
+};
