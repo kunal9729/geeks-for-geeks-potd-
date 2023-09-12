04f80440ab48b2fb40692c5fc60765d8d55bc4dd
Kunal Sabharwal <ksabharwal77@gmail.com>
2023-09-12 22:12:30 +0530

    Create Perfect Numbers.cpp

---
+++ Perfect Numbers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int isPerfectNumber(long long N) {
+        if(N == 1)
+            return 0;
+            
+        long long sum = 1;
+        
+        for(int i = 2; i <= sqrt(N); i++){
+            if(N % i == 0){
+                sum += i;
+                if(i != N / i)
+                    sum += N / i;
+            }
+        }
+        
+        return sum == N;
+    }
+};
